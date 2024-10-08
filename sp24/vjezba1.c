81194d9f37a9507739df908b618ade760cd29f85
KCuic539 <KCuic00@fesb.hr>
2024-10-08 18:50:58 +0200

    dio zadatka

--- sp24/vjezba1.c
+++ sp24/vjezba1.c
@@ -1,8 +1,33 @@
+#define _CRT_SECURE_NO_WARNINGS 
 #include<stdio.h>
+#define  ERROR_OPENING_FILE - 1;
+#define BUFFER_SIZE 1024;
+int count_rows(char* filename);
 
 int main()
 {
-	printf("hello\n");
+	
+
+	return 0;
+}
+int count_rows(char* filename)
+{
+	FILE* fp = NULL;
+	fp = fopen(filename, "r");
+	if (fp == NULL)
+	{
+		printf("error opening file!\n");
+		return ERROR_OPENING_FILE;
+	}
+
+	while (!feof(fp))
+	{
+		fgets(buffer, BUFFER_SIZE, fp);
+		++count;
+	}
+
+
+
 
 	return 0;
 }
